6ee5f492cff4feeae1915e5dc7916db40344cc76
natureNaaa <naturenaaa@gmail.com>
2024-07-31 12:45:53 +0800

    Update config.js

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   SIMPLE_LOGO_IMG: '/Logo.webp',
   SIMPLE_TOP_BAR: true, // 显示顶栏
   SIMPLE_TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
-  SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>RESEARCH INTEREST<br/>·Clinical Psychology<br/>·Adolescents and Children<br/>·Depression, ADHD, Autism<br/>·Neural Mechanism<br/>·Mindfulness Intervention</div>',
+  SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div style="text-align: left;"></div>RESEARCH INTEREST<br/>·Clinical Psychology<br/>·Adolescents and Children<br/>·Depression, ADHD, Autism<br/>·Neural Mechanism<br/>·Mindfulness Intervention</div>',
 
   SIMPLE_AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '#',
 
